6f9374e5960ad8773b8f72ca27aa56021960283e
emorina1 <151447538+emorina1@users.noreply.github.com>
2025-01-11 21:37:53 +0100

    Add project files.

---
+++ Universal-Hospital/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Universal_Hospital.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
