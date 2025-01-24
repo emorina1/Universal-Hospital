8fe2435ff3336d417114f3fd3f3a7a87632f0097
emorina1 <151447538+emorina1@users.noreply.github.com>
2025-01-24 15:23:08 +0100

    change views

--- Universal-Hospital/Models/Departament.cs
+++ Universal-Hospital/Models/Departament.cs
@@ -1,10 +1,20 @@
-﻿namespace Universal_Hospital.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Universal_Hospital.Models
 {
     public class Departament
     {
-        public int DepartamentId { get; set; } 
-        public string Name { get; set; } 
-        public string Description { get; set; } 
-        public int Floor { get; set; } 
+        [Key]
+        public int DepartamentId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Range(1, 10)] // Përcaktoni një gamë të arsyeshme për katin
+        public int Floor { get; set; }
     }
 }
